860d853e508b9cf66bc59e568857a806c46c1bc6
Aaron Vickers <aaron_vickers@hotmail.co.uk>
2021-11-18 22:52:19 +0000

    User class

---
+++ User.cpp
@@ -0,0 +1,22 @@
+// Include header file
+#include "User.h"
+
+std::string User::getUsername() {
+    // Return username
+    return username;
+}
+
+std::string User::getEmail() {
+    // Return email
+    return email;
+}
+
+std::string User::getSkill() {
+    // Return skill
+    return skill;
+}
+
+std::string User::getRole() {
+    // Return role
+    return role;
+}
