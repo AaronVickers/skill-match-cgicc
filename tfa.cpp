06adaaf00c9f4f0866d4c7ce1cc99dfafaa17b73
Aaron Vickers <aaron_vickers@hotmail.co.uk>
2021-11-18 22:31:47 +0000

    Two-factor authentication page

---
+++ tfa.cpp
@@ -0,0 +1,58 @@
+// Initialised headers
+#include "CgiccInit.h"
+#include "MySqlInit.h"
+
+// CGICC headers
+#include "cgicc/HTTPHTMLHeader.h"
+#include "cgicc/HTMLClasses.h"
+
+// Required headers
+#include "Authentication.h"
+
+// Use required namespaces
+using namespace std;
+using namespace cgicc;
+
+// Entry function
+int main(int argc, char *argv[]) {
+    // Initialise CGICC environment
+    CgiccInit cgi = CgiccInit();
+
+    // Required response data
+    cout << HTTPHTMLHeader() << endl;
+    cout << html() << head(title("Two-Factor Authentication")) << endl;
+    cout << body();
+
+    // Form for 2FA
+    auto TFAForm = form();
+    TFAForm.set("method", "post"); // Submit as post request
+
+    // Code input
+    TFAForm.add(cgicc::div()
+        .add(span("Code: "))
+        .add(input()
+            .set("type", "text")
+            .set("name", "code")
+            .set("placeholder", "Code")
+        )
+    );
+
+    // Submit button
+    TFAForm.add(input()
+        .set("type", "submit")
+        .set("value", "Submit")
+    );
+
+    // Login link
+    TFAForm.add(a("Login")
+        .set("href", "./login.cgi")
+    );
+
+    // Display 2FA form
+    cout << TFAForm;
+
+    // End of response
+    cout << body() << html();
+
+    return 0;
+}
