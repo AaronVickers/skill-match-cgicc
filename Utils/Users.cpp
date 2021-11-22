10558fb7ced1af34e6c407ead2294b058c9b63df
Aaron Vickers <aaron_vickers@hotmail.co.uk>
2021-11-22 08:21:14 +0000

    Implement Users::getUserByUsername

--- Utils/Users.cpp
+++ Utils/Users.cpp
@@ -54,8 +54,84 @@ User::User(std::string _username, std::string _email, std::string password, Role
 
 }
 
-UserResult Users::getUserByUsername(std::string username) {
+User::User(int _userId, std::string _username, std::string _email, std::string _passwordHashEncoded, int _roleId) {
+    userId = _userId;
+    username = _username;
+    email = _email;
+    passwordHashEncoded = _passwordHashEncoded;
+    roleId = _roleId;
+}
 
+UserResult Users::getUserByUsername(std::string username) {
+    // Create result
+    UserResult userResult = UserResult();
+
+    // Initialise MySQL connection
+    MySqlInit db = MySqlInit();
+
+    // Handle connection error
+    if (!db.getSuccess()) {
+        userResult.setError(db.getErrorMsg());
+
+        // Return result
+        return userResult;
+    }
+
+    // Attempt to complete operation
+    try {
+        // SQL statement variable
+        sql::PreparedStatement *pstmt;
+        // SQL result variable
+        sql::ResultSet *res;
+
+        // Prepare role select statement
+        pstmt = db.conn->prepareStatement("SELECT * FROM Users WHERE Username=?");
+
+        // Execute query
+        pstmt->setValue(1, username);
+        res = pstmt->executeQuery();
+
+        // Check if result has any rows
+        bool foundRow = res->next();
+
+        // Delete statement from memory
+        delete pstmt;
+
+        // Error if no rows selected
+        if (!res->next()) {
+            // Delete result from memory
+            delete res;
+
+            userResult.setError("User with provided username doesn't exist.");
+
+            return userResult;
+        }
+
+        // Get user details from first row
+        int userId = res->getInt("UserId");
+        std::string username = res->getValue("Username");
+        std::string email = res->getValue("Email");
+        std::string passwordHashEncoded = res->getValue("PasswordHashEncoded");
+        std::string roleId = res->getInt("RoleId");
+
+        // Store role in result
+        userResult.role = new User(userId, username, email, passwordHashEncoded, roleId);
+
+        // Delete result from memory
+        delete res;
+    } catch (sql::SQLException &sql_error) {
+        // Handle SQL exception
+        userResult.setError(sql_error.what());
+
+        // Return result
+        return userResult;
+    }
+
+    // Set result success
+    userResult.setSuccess(true);
+
+    // Return result
+    return userResult;
 }
 
 bool Users::doesAdminExist() {
--- Utils/Users.hpp
+++ Utils/Users.hpp
@@ -28,6 +28,7 @@ public:
 
     User(int _userId);
     User(std::string _username, std::string _email, std::string password, Role &role);
+    User(int _userId, std::string _username, std::string _email, std::string password, int _roleId);
 };
 
 // User result class structure
