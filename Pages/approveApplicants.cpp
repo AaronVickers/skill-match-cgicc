73112292d788c0bccdd577e4c6cb6c2c765a1088
Aaron Vickers <aaron_vickers@hotmail.co.uk>
2021-12-06 10:29:01 +0000

    Approve applicants and companies implementation

--- Pages/approveApplicants.cpp
+++ Pages/approveApplicants.cpp
@@ -1,6 +1,9 @@
 // CGI page header
 #include "Utils/CGIPage.hpp"
 
+// Initialised CGICC header
+#include "Utils/CGICCInit.hpp"
+
 // Initialised MariaDB header
 #include "Utils/MariaDBInit.hpp"
 
@@ -18,6 +21,7 @@
 // Required headers
 #include <ostream>
 #include "Utils/Authentication.hpp"
+#include "Utils/Skills.hpp"
 
 // Use required namespaces
 using namespace std;
@@ -77,19 +81,42 @@ void ApproveApplicantsCGIPage::onPOST(ostream &os) const {
         return;
     }
 
-    // TODO: Approve supplied applicants
+    // Approved and failed counts
+    int approvedCount = 0;
+    int failedCount = 0;
 
-    // Required response data
-    os << HTTPHTMLHeader() << endl;
-    os << html() << head(title("Approve Applicants")) << endl;
-    os << body();
+    // Get submitted form elements
+    const vector<FormEntry> formElements = CGICCInit::cgi->getElements();
 
-    // Temporary response
-    os << h1("Approve Applicants");
-    os << p("POST request not implemented yet.");
+    // Loop over all form elements
+    for (auto &formEntry: formElements) {
+        // Check if name of element is approve
+        if (formEntry.getName().compare("approve") == 0) {
+            // Attempt to approve skill search
+            try {
+                // Get skill search ID integer from value of element
+                int skillSearchId = stoi(formEntry.getValue());
 
-    // End of response
-    os << body() << html();
+                // Get skill search by skill search ID
+                SkillSearch skillSearch = SkillSearch(skillSearchId);
+
+                // Approve skill search
+                skillSearch.setApprovedState(true);
+
+                // Increase approved count
+                approvedCount++;
+            } catch(exception &e) {
+                // Increase failed count
+                failedCount++;
+            }
+        }
+    }
+
+    // Redirect location string with approved and failed counts
+    string redirectLocation = "./approveApplicants.cgi?approved=" + to_string(approvedCount) + "&failed=" + to_string(failedCount);
+
+    // Redirect to approve applicants page
+    os << HTTPRedirectHeader(redirectLocation, false) << endl;
 }
 
 // Entry function
